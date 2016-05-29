6c0736b7af42b9a537a4d636b6ea215c1c9adff1
ink0101 <cftt2016@gmail.com>
2016-05-29 19:01:22 +0800

    separate js from html

--- javascript/showPic.js
+++ javascript/showPic.js
@@ -1,8 +1,43 @@
+	//共享onload事件
+function addLoadEvent(func){
+	var oldonload = window.onload;
+	if (typeof window.onload!='function') {
+		window.onload = func;
+	}else{
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}
+
+addLoadEvent(prepareGallery);
+//绑定onload事件
+function prepareGallery(){
+	if (!document.getElementsByTagName) {
+		return false;
+	}
+	if (!document.getElementById) {
+		return false;
+	}
+	if (!document.getElementById('imagegallery')) {
+		return false;
+	}
+	var gallery = document.getElementById('imagegallery');
+	var links = gallery.getElementsByTagName('a');
+	for(var i=0;i<links.length;i++){
+		links[i].onclick=function(){
+			showPic(this);
+			return false;
+		}
+	}
+}
+//显示图片
 function showPic(whichpic) {
 	var source = whichpic.getAttribute('href');
 	var placeholder = document.getElementById('placeholder');
 	placeholder.setAttribute('src',source);
 	var title = whichpic.getAttribute('title');
-	var description = document.getElementsByTagName('p')[0].firstChild;
+	var description = document.getElementById('description').firstChild;
 	description.nodeValue=title;
 }
